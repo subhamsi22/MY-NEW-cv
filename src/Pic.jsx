c602414ac41752b7ad97c4cae6493be5c989a5fa
subham singh <159029907+subhamsi22@users.noreply.github.com>
2025-10-08 16:48:56 +0530

    Pic.jsx

--- src/Pic.jsx
+++ src/Pic.jsx
@@ -27,9 +27,10 @@ export function Pic() {
 
   return (
     <div>
-      <video id="gima" src="/pic/jk.mp4" controls autoPlay loop muted></video>
+      <video id="gima" src="/public/jk.mp4" controls autoPlay loop muted></video>
       <FontAwesomeIcon id="myloo" icon={faHtml5} size="3x" />
       <FontAwesomeIcon id="myjs" icon={faJs} size="3x" />
     </div>
   );
 }
+
